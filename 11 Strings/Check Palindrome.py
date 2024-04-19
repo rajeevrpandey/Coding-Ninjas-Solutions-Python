2a65522fc04b0e4739ae7ad673e31cb06e2dc90b
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2024-04-19 15:14:41 +0530

    Create Check Palindrome.py

---
+++ 11 Strings/Check Palindrome.py
@@ -0,0 +1,36 @@
+'''
+Problem statement
+Given a string, determine if it is a palindrome, considering only alphanumeric characters.
+
+Palindrome
+A palindrome is a word, number, phrase, or other sequences of characters which read the same backwards and forwards.
+Example:
+If the input string happens to be, "malayalam" then as we see that this word can be read the same as forward and backwards, it is said to be a valid palindrome.
+
+The expected output for this example will print, 'true'.
+From that being said, you are required to return a boolean value from the function that has been asked to implement.
+
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints:
+0 <= N <= 10^6
+Where N is the length of the input string.
+
+Time Limit: 1 second
+Sample Input 1 :
+abcdcba
+Sample Output 1 :
+true 
+Sample Input 2:
+coding
+Sample Output 2:
+false
+'''
+
+# Read input as sepcified in the question
+st = input()
+# Print output as specified in the question
+rev = st[::-1]
+if rev == st:
+    print("true")
+else:
+    print("false")
