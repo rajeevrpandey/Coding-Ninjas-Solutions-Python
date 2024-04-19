8f71a2a2478c2a78e627e1ce8920408680cd9b58
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2024-04-19 10:29:28 +0530

    Create Print Multiple Lines.py

---
+++ 01 Introduction to Python/Print Multiple Lines.py
@@ -0,0 +1,10 @@
+'''
+Problem statement
+Your task is to print these statement:
+
+I'm learning Python.
+Python is a high-level language.
+'''
+
+print("I’m learning Python.")
+print("Python is a high-level language.")
