77dd44da1a089f428a007755aa2af10a4265b48d
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2024-04-19 14:46:06 +0530

    Create Sum of two arrays.py

---
+++ 10 Searching & Sorting/Sum of two arrays.py
@@ -0,0 +1,123 @@
+'''
+Problem statement
+Two random integer arrays/lists have been given as ARR1 and ARR2 of size N and M respectively. Both the arrays/lists contain numbers from 0 to 9(i.e. single digit integer is present at every index). The idea here is to represent each array/list as an integer in itself of digits N and M.
+
+You need to find the sum of both the input arrays/list treating them as two integers and put the result in another array/list i.e. output array/list will also contain only single digit at every index.
+
+Note:
+The sizes N and M can be different. 
+
+Output array/list(of all 0s) has been provided as a function argument. Its size will always be one more than the size of the bigger array/list. Place 0 at the 0th index if there is no carry. 
+
+No need to print the elements of the output array/list.
+Using the function "sumOfTwoArrays", write the solution to the problem and store the answer inside this output array/list. The main code will handle the printing of the output on its own.
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints :
+1 <= t <= 10^2
+0 <= N <= 10^5
+0 <= M <= 10^5
+Time Limit: 1 sec 
+Sample Input 1:
+1
+3
+6 2 4
+3
+7 5 6
+Sample Output 1:
+1 3 8 0
+Sample Input 2:
+2
+3
+8 5 2
+2
+1 3
+4
+9 7 6 1
+3
+4 5 9
+Sample Output 2:
+0 8 6 5
+1 0 2 2 0 
+'''
+
+from sys import stdin
+
+def sumOfTwoArrays(arr1, n, arr2, m, output) :
+    #Your code goes here
+    n1 = 0
+    n2 = 0
+    for i in range(n-1,-1,-1):
+        n1 = n1*10 + arr1[i]
+    for j in range(m-1,-1,-1):
+        n2 = n2*10 + arr2[j]
+
+    print(n1, n2)
+    n3 = n1 + n2
+    output[0]=0
+    l3  = len(output)
+    for i in range(l3-1, -1, -1):
+        dig = n3%10
+        output.insert(i, dig)
+        n3//=10
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Taking Input Using Fast I/O
+def takeInput() :
+    n = int(stdin.readline().rstrip())
+    if n == 0 :
+        return list(), 0
+    
+    arr = list(map(int, stdin.readline().rstrip().split(" ")))
+    return arr, n
+
+
+#to print the array/list
+def printList(arr, n) :
+    for i in range(n) :
+        print(arr[i], end = " ")
+    
+    print()
+
+
+#main
+t = int(stdin.readline().rstrip())
+
+while t > 0 :
+    arr1, n = takeInput()
+    arr2, m = takeInput()
+    
+    outputSize = (1 + max(n, m))
+    output = outputSize * [0]
+    
+    sumOfTwoArrays(arr1, n, arr2, m, output)
+    printList(output, outputSize)
+    
+    t -= 1
