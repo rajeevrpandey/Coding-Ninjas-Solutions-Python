13ea2f8b64ba49e14fe7f249733bb95b047c5278
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2024-04-19 11:29:32 +0530

    Create Reverse number pattern.py

---
+++ 05 Patterns1/Reverse number pattern.py
@@ -0,0 +1,38 @@
+'''
+Problem statement
+Print the following pattern for the given N number of rows.
+
+Pattern for N = 4
+1
+21
+321
+4321
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints
+0 <= N <= 50
+Sample Input 1:
+5
+Sample Output 1:
+1
+21
+321
+4321
+54321
+Sample Input 2:
+6
+Sample Output 2:
+1
+21
+321
+4321
+54321
+654321
+'''
+
+## Read input as specified in the question
+n = int(input())
+## Print the required output in given format
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(i-j+1,end="")
+    print()
