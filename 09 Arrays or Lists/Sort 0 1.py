4b3239e7c6a49feb8de73de60dac17db25b8d600
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2024-04-19 14:27:23 +0530

    Create Sort 0 1.py

---
+++ 09 Arrays or Lists/Sort 0 1.py
@@ -0,0 +1,39 @@
+'''
+Problem statement
+You have been given an integer array/list(ARR) of size N that contains only integers, 0 and 1. Write a function to sort this array/list. Think of a solution which scans the array/list only once and don't require use of an extra array/list.
+
+Note:
+You need to change in the given array/list itself. Hence, no need to return or print anything. 
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints :
+1 <= t <= 10^2
+0 <= N <= 10^5
+Time Limit: 1 sec
+Sample Input 1:
+1
+7
+0 1 1 0 1 0 1
+Sample Output 1:
+0 0 0 1 1 1 1
+Sample Input 2:
+2
+8
+1 0 1 1 0 1 0 1
+5
+0 1 0 1 0
+Sample Output 2:
+0 0 0 1 1 1 1 1
+0 0 0 1 1 
+'''
+
+from sys import stdin
+
+def sortZeroesAndOne(arr, n) :
+    #Your code goes here 
+    ptr1 = 0
+    for ptr0 in range(n):
+        if arr[ptr0]==0:
+            arr[ptr0],arr[ptr1] = arr[ptr1],arr[ptr0]
+            ptr1 += 1
+
+
