09f691d7cf583994b286880d0e27135c55d0c7ce
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2024-04-19 14:43:28 +0530

    Create Second Largest in array.py

---
+++ 10 Searching & Sorting/Second Largest in array.py
@@ -0,0 +1,55 @@
+'''
+Problem statement
+You have been given a random integer array/list(ARR) of size N. You are required to find and return the second largest element present in the array/list.
+
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints :
+0 <= N <= 10^2
+1<=arr[i]<=10^3
+
+Time Limit: 1 sec
+Sample Input 1:
+5
+4 3 10 9 2
+Sample Output 1:
+9
+Sample Input 2:
+7
+13 6 31 14 29 44 3
+Sample Output 2:
+31
+'''
+
+from sys import stdin
+MIN_VALUE = -2147483648
+
+
+def secondLargestElement(arr, n):
+    first = MIN_VALUE
+    second = MIN_VALUE
+    for i in range(n):
+        if arr[i]>=first:
+            second = first
+            first = arr[i]
+        elif(arr[i]>=second):
+            second  = arr[i]
+    return second
+
+    
+
+
+
+def takeInput() :
+    n = int(stdin.readline().rstrip())
+    if n != 0:
+        arr = list(map(int, stdin.readline().rstrip().split(" ")))
+        return arr, n
+
+    return list(), 0
+
+
+#main
+arr, n = takeInput()
+print(secondLargestElement(arr, n))
+
+
