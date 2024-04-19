18d147f82cafa80373f51cfeea7ce8fba3f0a2eb
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2024-04-19 14:42:31 +0530

    Create Rotate array.py

---
+++ 10 Searching & Sorting/Rotate array.py
@@ -0,0 +1,43 @@
+'''
+Problem statement
+You have been given a random integer array/list(ARR) of size N. Write a function that rotates the given array/list by D elements(towards the left).
+
+ Note:
+Change in the input array/list itself.You don't need to return or print the elements.
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints :
+1 <= t <= 10^4
+0 <= N <= 10^6
+0 <= D <= N
+Time Limit: 1 sec
+Sample Input 1:
+1
+7
+1 2 3 4 5 6 7
+2
+Sample Output 1:
+3 4 5 6 7 1 2
+Sample Input 2:
+2
+7
+1 2 3 4 5 6 7
+0
+4
+1 2 3 4
+2
+Sample Output 2:
+1 2 3 4 5 6 7
+3 4 1 2
+'''
+
+from sys import stdin
+
+
+def rotate(arr, n, d):
+    #Your code goes here
+    ans = [0]*n
+    for i in range(n):
+        ans[i-d] = arr[i]
+    for i in range(n):
+        arr[i] = ans[i]
+
