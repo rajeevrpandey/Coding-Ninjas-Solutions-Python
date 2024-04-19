476f9cf640003510767c2f51755de09053613796
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2024-04-19 15:16:48 +0530

    Create Remove Consecutive Duplicates.py

---
+++ 11 Strings/Remove Consecutive Duplicates.py
@@ -0,0 +1,49 @@
+'''
+Problem statement
+For a given string(str), remove all the consecutive duplicate characters.
+
+Example:
+Input String: "aaaa"
+Expected Output: "a"
+
+Input String: "aabbbcc"
+Expected Output: "abc"
+ Input Format:
+The first and only line of input contains a string without any leading and trailing spaces. All the characters in the string would be in lower case.
+Output Format:
+The only line of output prints the updated string.
+Note:
+You are not required to print anything. It has already been taken care of.
+Constraints:
+0 <= N <= 10^6
+Where N is the length of the input string.
+
+Time Limit: 1 second
+Constraints:
+0 <= N <= 10^6
+Where N is the length of the input string.
+
+Time Limit: 1 second
+Sample Input 1:
+aabccbaa
+Sample Output 1:
+abcba
+Sample Input 2:
+xxyyzxx
+Sample Output 2:
+xyzx
+
+'''
+
+from sys import stdin
+
+def removeConsecutiveDuplicates(string) :
+	# Your code goes here
+	n=len(string)
+	newstr = ""
+	newstr += string[0]
+	for i in range(1,len(string)):
+		if string[i]!=string[i-1]:
+			newstr += string[i]
+	return newstr
+
