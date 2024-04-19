1888cfb06653e8eaa3ce81c71563eba8632facdc
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2024-04-19 15:43:33 +0530

    Create Transpose of a matrix.py

---
+++ 2D Arrays or 2D Lists/Transpose of a matrix.py
@@ -0,0 +1,70 @@
+'''
+Problem statement
+You are given a matrix ‘MAT’. Print and return the transpose of the matrix. The transpose of a matrix is obtained by changing rows to columns and columns to rows. In other words, transpose of a matrix A[][] is obtained by changing A[i][j] to A[j][i].
+
+For example:
+Let matrix be : 
+1 2
+3 4
+
+Then transpose of the matrix will be: 
+1 3
+2 4
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints :
+1 <= T <= 10
+1 <= M, N <= 3*10^3
+0 <= MAT[i][j] <= 10^7
+
+Time Limit: 1 sec
+Sample Input 1 :
+2
+2 2
+1 2
+3 4
+3 2
+1 2 
+2 3
+3 4
+Sample Output 1 :
+1 3
+2 4
+1 2 3
+2 3 4
+Explanation For Sample Input 1 :
+For test case 1: 
+The transpose of the matrix will be: 
+1 3
+2 4
+
+For test case 2:    
+The transpose of the matrix will be: 
+1 2 3
+2 3 4
+Sample Input 2 :
+2
+2 3
+1 2 3 
+3 4 5
+2 1
+1
+2
+Sample Output 2 :
+1 3
+2 4 
+3 5
+1 2
+'''
+
+
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+from typing import List
+
+def transpose(m: int, n: int, mat: List[List[int]]) -> List[List[int]]:
+    newmat = [[0 for i in range(m)]for j in range(n)]
+    for i in range(m):
+
