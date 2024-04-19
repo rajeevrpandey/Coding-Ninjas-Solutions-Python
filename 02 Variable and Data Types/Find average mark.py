84299ff527238de1590534c996788ff80bcce309
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2024-04-19 10:39:35 +0530

    Create Find average mark.py

---
+++ 02 Variable and Data Types/Find average mark.py
@@ -0,0 +1,25 @@
+'''
+Problem statement
+Write a program to input marks of three tests of a student (all integers). Then calculate and print the average of all test marks.
+
+Detailed explanation ( Input/output format, Notes, Images )
+Sample Input 1 :
+3 
+4 
+6
+Sample Output 1 :
+4.333333333333333
+Sample Input 2 :
+5 
+10 
+5
+Sample Output 2 :
+6.666666666666667
+'''
+
+# Read input as sepcified in the question
+x = int(input())
+y = int(input())
+z = int(input())
+# Print output as specified in the question
+print((x+y+z)/3)
