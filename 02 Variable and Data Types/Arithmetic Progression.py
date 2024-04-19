63e76c0d3f5abe653fcfdf786f2cb90dd295882b
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2024-04-19 10:45:09 +0530

    Create Arithmetic Progression.py

---
+++ 02 Variable and Data Types/Arithmetic Progression.py
@@ -0,0 +1,29 @@
+'''
+Problem statement
+You are given first three entries of an arithmetic progression. You have to calculate the common difference and print it.
+
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints:
+Time Limit: 1 second
+Output format:
+The first and only line of output contains the result.  
+Sample Input 1:
+1
+3
+5
+Sample Output 1:
+2
+Sample Input 2:
+100
+200
+300
+Sample Output 2:
+100
+'''
+
+# Write your code here
+a= int(input())
+b= int(input())
+c= int(input())
+
+print(b-a)
