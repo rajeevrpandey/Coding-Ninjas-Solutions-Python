c1a3ce87f535c9c56c6f4a96b885937cbbc9963e
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2024-04-19 10:52:50 +0530

    Create Bit Operations.py

---
+++ 03 Operators & Conditionals/Bit Operations.py
@@ -0,0 +1,24 @@
+'''
+Problem statement
+You are given two integers, a and b. Your task is to implement a program that performs the following operations using bitwise operators:
+
+1. Calculate the bitwise left shift (<<) of a by 1 position to the left.  
+2. Calculate the bitwise right shift (>>) of b by 1 position to the right.
+3.Print the results of each operation.
+Detailed explanation ( Input/output format, Notes, Images )
+Sample input :
+12
+7
+Sample output:
+24
+3
+'''
+
+a = 12  # Example value for 'a'
+b = 7   # Example value for 'b'
+
+# Bitwise left shift for 'a'
+print(a<<1)
+
+# Bitwise right shift for 'b'
+print(b>>1)
