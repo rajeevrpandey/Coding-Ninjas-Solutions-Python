695a5520ab798f12a47cc37f7cc322f6b64c0d52
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2024-04-21 16:49:05 +0530

    Create sum of array.py

---
+++ 15 Recursion 1/sum of array.py
@@ -0,0 +1,34 @@
+'''
+Problem statement
+Given an array of length N, you need to find and return the sum of all elements of the array.
+
+Do this recursively.
+
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints :
+1 <= N <= 10^3
+Sample Input 1 :
+3
+9 8 9
+Sample Output 1 :
+26
+Sample Input 2 :
+3
+4 2 1
+Sample Output 2 :
+7    
+
+'''
+
+
+def sumArray(arr):
+    if len(arr)==0:
+        return 0
+    return arr[0] + sumArray(arr[1:])
+
+# Main
+from sys import setrecursionlimit
+setrecursionlimit(11000)
+n=int(input())
+arr=list(int(i) for i in input().strip().split(' '))
+print(sumArray(arr))
