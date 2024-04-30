f7632b7d777c0eb9e421ba41b5bdbc4be977534b
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2024-04-30 08:36:27 +0530

    Create Check palindrome.py

---
+++ 17 Recursion assignment/Check palindrome.py
@@ -0,0 +1,31 @@
+'''
+Problem statement
+Determine if a given string ‘S’ is a palindrome using recursion. Return a Boolean value of true if it is a palindrome and false if it is not.
+
+Note: You are not required to print anything, just implement the function. Example:
+Input: s = "racecar"
+Output: true
+Explanation: "racecar" is a palindrome.
+Detailed explanation ( Input/output format, Notes, Images )
+Sample Input 1:
+abbba
+Sample Output 1:
+true
+Explanation Of Sample Input 1 :
+“abbba” is a palindrome
+Sample Input 2:
+abcd
+Sample Output 2:
+false
+Explanation Of Sample Input 2 :
+“abcd” is not a palindrome.
+Constraints:
+0 <= |S| <= 10^6
+where |S| represents the length of string S.
+'''
+def isPalindrome(str):
+    if len(str)<=1:
+        return True
+    if str[0]!=str[len(str)-1]:
+        return False
+    return isPalindrome(str[1:-1])
