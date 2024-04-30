ad4d875de2ef145e67309a8d7eea5a6d97a813af
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2024-04-30 08:25:39 +0530

    Create Merge Sort.py

---
+++ 16 Recursion 2/Merge Sort.py
@@ -0,0 +1,91 @@
+'''
+Problem statement
+You are given the starting 'l' and the ending 'r' positions of the array 'ARR'.
+
+
+
+You must sort the elements between 'l' and 'r'.
+
+
+
+Note:
+Change in the input array itself. So no need to return or print anything.
+Example:
+Input: ‘N’ = 7,
+'ARR' = [2, 13, 4, 1, 3, 6, 28]
+
+Output: [1 2 3 4 6 13 28]
+
+Explanation: After applying 'merge sort' on the input array, the output is [1 2 3 4 6 13 28].
+Detailed explanation ( Input/output format, Notes, Images )
+Sample Input 1:
+7
+2 13 4 1 3 6 28
+Sample Output 1:
+1 2 3 4 6 13 28
+Explanation of Sample Output 1:
+After applying 'merge sort' on the input array, the output is [1 2 3 4 6 13 28].
+Sample Input 2:
+5
+9 3 6 2 0
+Sample Output 2:
+0 2 3 6 9
+Explanation of Sample Output 2:
+After applying 'merge sort' on the input array, the output is [0 2 3 6 9].
+Constraints :
+1 <= N <= 10^3
+0 <= ARR[i] <= 10^9
+'''
+def mergeSort(arr: [int], l: int, r: int):
+    if l < r:
+        # Find the middle point to divide the array into two halves
+        m = (l + r) // 2
+
+        # Call mergeSort for the first half
+        mergeSort(arr, l, m)
+        # Call mergeSort for the second half
+        mergeSort(arr, m + 1, r)
+
+        # Merge the two halves
+        merge(arr, l, m, r)
+
+def merge(arr, l, m, r):
+    n1 = m - l + 1
+    n2 = r- m
+
+    # Create temp arrays
+    L = [0] * (n1)
+    R = [0] * (n2)
+
+    # Copy data to temp arrays L[] and R[]
+    for i in range(0 , n1):
+        L[i] = arr[l + i]
+
+    for j in range(0 , n2):
+        R[j] = arr[m + 1 + j]
+
+    # Merge the temp arrays back into arr[l..r]
+    i = 0    # Initial index of first subarray
+    j = 0    # Initial index of second subarray
+    k = l    # Initial index of merged subarray
+
+    while i < n1 and j < n2:
+        if L[i] <= R[j]:
+            arr[k] = L[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            j += 1
+        k += 1
+
+    # Copy the remaining elements of L[], if there are any
+    while i < n1:
+        arr[k] = L[i]
+        i += 1
+        k += 1
+
+    # Copy the remaining elements of R[], if there are any
+    while j < n2:
+        arr[k] = R[j]
+        j += 1
+        k += 1
