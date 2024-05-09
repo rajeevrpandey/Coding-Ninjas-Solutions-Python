6705e854fa2f285b7b3da6d66d8580a458ee49a3
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2024-05-09 15:39:06 +0530

    Create Array intersection.py

---
+++ 21 Applications of Complexity Analysis/Array intersection.py
@@ -0,0 +1,113 @@
+'''
+Problem statement
+You have been given two integer arrays/list(ARR1 and ARR2) of size N and M, respectively. You need to print their intersection; An intersection for this problem can be defined when both the arrays/lists contain a particular value or to put it in other words, when there is a common value that exists in both the arrays/lists.
+
+Note :
+Input arrays/lists can contain duplicate elements.
+
+The intersection elements printed would be in ascending order.
+
+
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints :
+1 <= t <= 10^2
+0 <= N <= 10^4
+0 <= M <= 10^4
+
+Time Limit: 1 sec 
+Sample Input 1 :
+2
+6
+2 6 8 5 4 3
+4
+2 3 4 7 
+2
+10 10
+1
+10
+Sample Output 1 :
+2 3 4
+10
+Sample Input 2 :
+1
+4
+2 6 1 2
+5
+1 2 3 4 2
+Sample Output 2 :
+1 2 2
+Explanation for Sample Output 2 :
+Since, both input arrays have two '2's, the intersection of the arrays also have two '2's. The first '2' of first array matches with the first '2' of the second array. Similarly, the second '2' of the first array matches with the second '2' if the second array.
+'''
+
+from sys import stdin
+
+
+def intersection(arr1, arr2, n, m) :
+
+    arr1.sort()
+    arr2.sort()
+
+    i = 0 #pointer to iterate over arr1
+    j = 0 #pointer to iterate over arr2
+
+    while i < n and j < m :
+        
+        if arr1[i] == arr2[j] :
+            print(arr1[i], end = " ") 
+            i += 1
+            j += 1
+        elif arr1[i] < arr2[j] :
+            i += 1
+        else :
+            j += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Taking input using fast I/O method
+def takeInput() :
+    n = int(stdin.readline().strip())
+    
+    if n == 0 :
+    	return list(), 0
+
+    arr = list(map(int, stdin.readline().strip().split(" ")))
+    return arr, n
+
+
+#main
+t = int(stdin.readline().strip())
+
+while t > 0 :
+
+    arr1, n = takeInput()
+    arr2, m = takeInput()
+    intersection(arr1, arr2, n, m)
+    print()
+
+    t -= 1
