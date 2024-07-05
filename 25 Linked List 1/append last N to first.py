6a64686556c6879be0c01633aeb8b9f0632c9b7b
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2024-07-05 14:05:07 +0530

    Create append last N to first.py

---
+++ 25 Linked List 1/append last N to first.py
@@ -0,0 +1,142 @@
+'''
+Problem statement
+You have been given a singly linked list of integers along with an integer 'N'. Write a function to append the last 'N' nodes towards the front of the singly linked list and returns the new head to the list.
+
+Hint:
+Identify how many pointers you require and try traversing them to right places and connect nodes accordingly also don't forget to disconnect what's required else it could create cycles.
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints :
+1 <= t <= 10^2
+0 <= M <= 10^5
+0 <= N < M
+Time Limit: 1sec
+
+Where 'M' is the size of the singly linked list.
+Remember/Consider :
+While specifying the list elements for input, -1 indicates the end of the singly linked list and hence, would never be a list element.
+Sample Input 1 :
+2
+1 2 3 4 5 -1
+3
+10 20 30 40 50 60 -1
+5
+Sample Output 1 :
+3 4 5 1 2
+20 30 40 50 60 10
+Sample Input 2 :
+1
+10 6 77 90 61 67 100 -1
+4
+Sample Output 2 :
+90 61 67 100 10 6 77 
+ Explanation to Sample Input 2 :
+We have been required to move the last 4 nodes to the front of the list. Here, "90->61->67->100" is the list which represents the last 4 nodes. When we move this list to the front then the remaining part of the initial list which is, "10->6->77" is attached after 100. Hence, the new list formed with an updated head pointing to 90.
+'''
+
+
+from sys import stdin
+
+#Following is the Node class already written for the Linked List
+class Node :
+    def __init__(self, data) :
+        self.data = data
+        self.next = None
+
+
+
+
+def appendLastNToFirst(head, n) :
+    if head==None or n<0:
+        return None
+    c=0
+    curr=head
+    prev=head
+    while curr is not None and n!=c:
+        c=c+1
+        curr=curr.next
+    if curr==None:
+        return None
+    next=curr.next
+    while next is not None:
+        curr=next
+        next=curr.next
+        prev=prev.next
+    curr.next=head
+    curr=prev.next
+    prev.next=None
+    return curr
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Taking Input Using Fast I/O
+def takeInput() :
+    head = None
+    tail = None
+
+    datas = list(map(int, stdin.readline().rstrip().split(" ")))
+
+    i = 0
+    while (i < len(datas)) and (datas[i] != -1) :
+        data = datas[i]
+        newNode = Node(data)
+
+        if head is None :
+            head = newNode
+            tail = newNode
+
+        else :
+            tail.next = newNode
+            tail = newNode
+
+        i += 1
+
+    return head
+
+
+#to print the linked list 
+def printLinkedList(head) :
+
+    while head is not None :
+        print(head.data, end = " ")
+        head = head.next
+
+    print()
+
+
+#main
+t = int(stdin.readline().rstrip())
+
+while t > 0 :
+
+    head = takeInput()
+    n = int(stdin.readline().rstrip())
+
+    head = appendLastNToFirst(head, n)
+    printLinkedList(head)
+
+    t -= 1 
+
+
