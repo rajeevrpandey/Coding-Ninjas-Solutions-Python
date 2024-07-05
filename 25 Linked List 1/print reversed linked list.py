e927dfbd6c1b7318a200787ebb25f0eb83068fac
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2024-07-05 14:07:05 +0530

    Create print reversed linked list.py

---
+++ 25 Linked List 1/print reversed linked list.py
@@ -0,0 +1,124 @@
+'''
+Problem statement
+You have been given a singly linked list of integers. Write a function to print the list in a reverse order.
+
+To explain it further, you need to start printing the data from the tail and move towards the head of the list, printing the head data at the end.
+
+Note :
+You can’t change any of the pointers in the linked list, just print the linked list in the reverse order.
+Detailed explanation ( Input/output format, Notes, Images )
+ Constraints :
+1 <= t <= 10^2
+0 <= M <= 10^3
+
+
+Time Limit: 1sec
+
+Where 'M' is the size of the singly linked list.
+Remember/Consider :
+While specifying the list elements for input, -1 indicates the end of the singly linked list and hence, would never be a list element.
+Sample Input 1 :
+1
+1 2 3 4 5 -1
+Sample Output 1 :
+5 4 3 2 1
+Sample Input 2 :
+2
+1 2 3 -1
+10 20 30 40 50 -1
+Sample Output 2 :
+3 2 1
+50 40 30 20 10
+'''
+from sys import stdin, setrecursionlimit
+setrecursionlimit(10 ** 6)
+
+#Following is the Node class already written for the Linked List
+class Node :
+    def __init__(self, data) :
+        self.data = data
+        self.next = None
+
+
+
+
+def printReverse(head) :
+    if head is None:
+        return None
+    printReverse(head.next)
+    print(head.data, end = " ")    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Taking Input Using Fast I/O
+def takeInput() :
+    head = None
+    tail = None
+
+    datas = list(map(int, stdin.readline().rstrip().split(" ")))
+
+    i = 0
+    while (i < len(datas)) and (datas[i] != -1) :
+        data = datas[i]
+        newNode = Node(data)
+
+        if head is None :
+            head = newNode
+            tail = newNode
+
+        else :
+            tail.next = newNode
+            tail = newNode
+
+        i += 1
+
+    return head
+
+
+#to print the linked list 
+def printLinkedList(head) :
+
+    while head is not None :
+        print(head.data, end = " ")
+        head = head.next
+
+    print()
+
+
+#main
+t = int(stdin.readline().rstrip())
+
+while t > 0 :
+
+    head = takeInput()
+    printReverse(head)
+    print()
+
+    t -= 1
