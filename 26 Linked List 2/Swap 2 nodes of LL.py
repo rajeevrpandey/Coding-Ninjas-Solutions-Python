665951f34489deb568afd01cff5ab05403a68ce8
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2024-07-06 11:55:22 +0530

    Create Swap 2 nodes of LL.py

---
+++ 26 Linked List 2/Swap 2 nodes of LL.py
@@ -0,0 +1,147 @@
+'''
+Problem statement
+You have been given a singly linked list of integers along with two integers, 'i,' and 'j.' Swap the nodes that are present at the 'i-th' and 'j-th' positions and return the new head to the list.
+
+Note :
+1. Remember, You need to swap the nodes, not only the data.
+2. Indexing starts from 0.
+3. No need to print the list, it has already been taken care.
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints :
+1 <= t <= 10^2
+0 <= M <= 10^5
+Where M is the size of the singly linked list.
+0 <= i < M
+0 <= j < M
+
+Time Limit: 1sec
+Sample Input 1 :
+1
+3 4 5 2 6 1 9 -1
+3 4
+Sample Output 1 :
+3 4 5 6 2 1 9 
+ Sample Input 2 :
+2
+10 20 30 40 -1
+1 2
+70 80 90 25 65 85 90 -1
+0 6
+ Sample Output 2 :
+10 30 20 40 
+90 80 90 25 65 85 70 
+'''
+from sys import stdin
+
+#Following is the Node class already written for the Linked List
+class Node :
+    def __init__(self, data) :
+        self.data = data
+        self.next = None
+
+
+
+def swapNodes(head, i, j) :
+    ptr1 = head
+    ptr2 = head
+    
+    cnt1 = 0
+    cnt2 = 0
+    
+    while cnt1 < i:
+        cnt1 += 1
+        ptr1 = ptr1.next
+        
+    while cnt2 < j:
+        cnt2 += 1
+        ptr2 = ptr2.next
+        
+    curr1 = ptr1
+    curr2 = ptr2
+    
+    data1 = curr1.data
+    data2 = curr2.data
+
+    curr1.data = data2
+    curr2.data = data1
+
+    return head
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Taking Input Using Fast I/O
+def takeInput() :
+    head = None
+    tail = None
+
+    datas = list(map(int, stdin.readline().rstrip().split(" ")))
+
+    i = 0
+    while (i < len(datas)) and (datas[i] != -1) :
+        data = datas[i]
+        newNode = Node(data)
+
+        if head is None :
+            head = newNode
+            tail = newNode
+
+        else :
+            tail.next = newNode
+            tail = newNode
+
+        i += 1
+
+    return head
+
+
+
+
+def printLinkedList(head) :
+
+    while head is not None :
+        print(head.data, end = " ")
+        head = head.next
+
+    print()
+
+
+#main
+t = int(stdin.readline().rstrip())
+
+while t > 0 :
+    
+    head = takeInput()
+    i_j = stdin.readline().strip().split(" ")
+
+    i = int(i_j[0])
+    j = int(i_j[1])
+
+    newHead = swapNodes(head, i, j)
+    printLinkedList(newHead)
+
+    t -= 1
